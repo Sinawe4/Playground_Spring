f5d4890595e405a7abbade59ba52de651a30e2e9
YongWoo <aba1430@naver.com>
2021-11-25 17:26:23 +0900

    [FIx} error

--- src/main/java/com/hackathon/playground/repository/PlayedTotalRepository.java
+++ src/main/java/com/hackathon/playground/repository/PlayedTotalRepository.java
@@ -1,7 +1,8 @@
 package com.hackathon.playground.repository;
 
+import com.hackathon.playground.domain.PlayTotalID;
 import com.hackathon.playground.domain.PlayedTotal;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface PlayedTotalRepository extends JpaRepository<PlayedTotal, Long> {
+public interface PlayedTotalRepository extends JpaRepository<PlayedTotal, PlayTotalID> {
 }
