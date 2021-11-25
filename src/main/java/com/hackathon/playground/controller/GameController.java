7be50907b0205a92749cb43a9c26193611f86345
YongWoo <aba1430@naver.com>
2021-11-25 16:04:42 +0900

    [ADD] Create GameController

---
+++ src/main/java/com/hackathon/playground/controller/GameController.java
@@ -0,0 +1,25 @@
+package com.hackathon.playground.controller;
+
+import com.hackathon.playground.domain.response.CommonResult;
+import com.hackathon.playground.domain.response.ResponseService;
+import com.hackathon.playground.dto.GameEndDto;
+import com.hackathon.playground.service.GameService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/game")
+public class GameController {
+
+    private final ResponseService responseService;
+    private final GameService gameService;
+
+    @PostMapping("/end")
+    public CommonResult gameEnd(GameEndDto gameEndDto){
+        gameService.GameEnd(gameEndDto);
+        return responseService.getSuccessResult();
+    }
+}
