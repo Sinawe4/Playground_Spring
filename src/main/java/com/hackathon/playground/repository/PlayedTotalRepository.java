1d3d8900d36c0fbb4624a1fd2b15acaa4ac556a0
YongWoo <aba1430@naver.com>
2021-11-25 16:07:58 +0900

    [ADD] Create Repo

---
+++ src/main/java/com/hackathon/playground/repository/PlayedTotalRepository.java
@@ -0,0 +1,7 @@
+package com.hackathon.playground.repository;
+
+import com.hackathon.playground.domain.PlayedTotal;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PlayedTotalRepository extends JpaRepository<PlayedTotal, Long> {
+}
