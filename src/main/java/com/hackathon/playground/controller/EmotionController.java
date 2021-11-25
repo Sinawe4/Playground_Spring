93b6a1b21226fef5b7c99321a13c12e075fc4e70
YongWoo <aba1430@naver.com>
2021-11-25 16:19:13 +0900

    [ADD] Create Emotion Controller

---
+++ src/main/java/com/hackathon/playground/controller/EmotionController.java
@@ -0,0 +1,26 @@
+package com.hackathon.playground.controller;
+
+import com.hackathon.playground.domain.response.CommonResult;
+import com.hackathon.playground.domain.response.ResponseService;
+import com.hackathon.playground.dto.EmotionDto;
+import com.hackathon.playground.dto.GameEndDto;
+import com.hackathon.playground.service.EmotionService;
+import com.hackathon.playground.service.GameService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/emotion")
+public class EmotionController {
+    private final ResponseService responseService;
+    private final EmotionService emotionService;
+
+    @PostMapping("/save")
+    public CommonResult emotionSave(EmotionDto emotionDto){
+        emotionService.emotionSave(emotionDto);
+        return responseService.getSuccessResult();
+    }
+}
